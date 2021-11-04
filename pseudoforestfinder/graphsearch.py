d5b699f9798db755a84c937b204a45e3d4bd3ccb
iori <andrea.saracino@iit.cnr.it>
2021-11-04 20:43:22 +0100

    Added some comments on pseudoforestfinder/graphsearch.py

--- pseudoforestfinder/graphsearch.py
+++ pseudoforestfinder/graphsearch.py
@@ -25,19 +25,20 @@ def find_all_paths(graph, start_node, stable_list):
             #else: print("not in stable list")
     return path_list
 
+#Variation of the find_all_paths which exploits set intersection instead of iterative search (faster but requires more memory)
 def find_all_paths_var(graph, start_node, stable_list):
     path_list = []
     successorslist = list(graph.successors(start_node))
     predecessorslist = list(graph.predecessors(start_node))
-    print("number of successors: ", len(successorslist)) #green arrows
-    print("number of predecessors: ", len(predecessorslist)) #blue arrows
-    usable_nodes = list(set(stable_list) & set(predecessorslist))
+    print("number of successors: ", len(successorslist)) #green arrows - forward path
+    print("number of predecessors: ", len(predecessorslist)) #blue arrows - reverese path
+    usable_nodes = list(set(stable_list) & set(predecessorslist)) #filter blue arrows on stable nodes
     for stable_node in usable_nodes:
         for second_node in successorslist:
-            if stable_node != second_node:
-                tc_nodes = list(set(list(graph.successors(second_node))) & set(list(graph.predecessors(stable_node))))
+            if stable_node != second_node: #avoid cycling on two nodes iteratively
+                tc_nodes = list(set(list(graph.successors(second_node))) & set(list(graph.predecessors(stable_node)))) #search the nodes connecting the forward and reverse path
                 print ("The list of tc nodes is: ", tc_nodes)
-                for tc_node in tc_nodes:
+                for tc_node in tc_nodes: #if the tc nodes exist, save the paths
                     path = [start_node, second_node, tc_node, stable_node, start_node]
                     path_list.append(path)
     return path_list
@@ -77,14 +78,14 @@ edges = G.add_edge(5,6,weight=1)
 edges = G.add_edge(6,1,weight=1)
 
 
-print("number of nodes: ", G.number_of_nodes())
-print("number of edges: ", G.number_of_edges())
+print("number of nodes: ", the_graph.number_of_nodes())
+print("number of edges: ", the_graph.number_of_edges())
 #for line in nx.generate_edgelist(the_graph):
 #   if line[0] == 2:
 #       print(line)
     
-stable_nodes = [3,4,6]
-start_node = 2 
+stable_nodes = [2,4,6]
+start_node = 3 
 #print(nx.is_directed(G))
 #nx.draw(the_graph, pos=nx.circular_layout(the_graph), node_color='r', edge_color='b') #draw graph 
 #pred=G.predecessors(1)
